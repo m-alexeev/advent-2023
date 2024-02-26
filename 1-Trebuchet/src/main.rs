4f6d93e6ecd368ae6790e56e2c09857c4985212b
Misha <mishalxv35@gmail.com>
2024-02-25 19:29:36 -0800

    fix: file read into a string vector

--- 1-Trebuchet/src/main.rs
+++ 1-Trebuchet/src/main.rs
@@ -2,10 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-
-fn part1(buffer: &mut io::Lines<io::BufReader<File>>) -> u32 {
+fn part1(buffer: &Vec<String>) -> u32 {
     let mut running_sum: u32 = 0;
-    for line in buffer.flatten(){
+    for line in buffer {
         // Find the leftmost numeric character in the line
         let left = line.chars().find(|ch| ch.is_numeric());
 
@@ -20,35 +19,45 @@ fn part1(buffer: &mut io::Lines<io::BufReader<File>>) -> u32 {
             println!("No numeric characters found in the line.");
         }
     }
-    return  running_sum;
+    return running_sum;
 }
 
-fn part2(buffer: &mut io::Lines<io::BufReader<File>>) -> u32 {
+fn part2(buffer: &Vec<String>) -> u32 {
     let running_sum: u32 = 0;
 
-    let digits = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    for line in buffer.flatten(){
-        let mut parsed_digits: Vec<u32> = vec![];
-        for digit in digits.iter(){
-        }
+    let _digits = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    for line in buffer {
+        // search until first digit can be represented by a number or a string
+        // Find the leftmost numeric character in the line
+        let left = line.chars().find(|ch| ch.is_numeric());
+
+        // Find the rightmost numeric character in the line
+        // let right = line.chars().rev().find(|ch| ch.is_numeric());
+        //
+        // let mut parsed_digits: Vec<u32> = vec![];
+        // for digit in digits.iter() {}
     }
     return running_sum;
 }
 
-
 fn main() {
     // File hosts.txt must exist in the current path
-    if let Ok(mut lines) = read_lines("./sample") {
+    if let Ok(lines) = read_lines("./sample-2") {
         // Consumes the iterator, returns an (Optional) String
-        println!("{}", part1(&mut lines));
-        println!("{}", part2(&mut lines));
+        let buffer: Vec<String> = lines.flatten().map(String::from).collect();
+        println!("{}", part1(&buffer));
+        println!("{}", part2(&buffer));
     }
 }
 
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
