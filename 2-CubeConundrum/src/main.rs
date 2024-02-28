c8ed925041fd9efb64e1384d3540e5ca2d41ac2a
Mikhail Alexeev <mishalxv35@gmail.com>
2024-02-27 20:17:01 -0800

    cubeconudrum: part 1 working on sample input

--- 2-CubeConundrum/src/main.rs
+++ 2-CubeConundrum/src/main.rs
@@ -2,22 +2,86 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-fn part1(buffer: &Vec<String>) -> u32 {
-    for line in buffer{
-        let game_str= line.split(":").collect::<Vec<&str>>()[0];
-        let game_id = game_str.split(" ").collect::<Vec<&str>>()[1];
-            
-        println!("{:?}", game_id.parse::<u32>().unwrap());
-    }
-    0
+#[derive(Debug)]
+#[derive(PartialEq)]
+
+enum Color {
+    Red,
+    Green,
+    Blue,
 }
+
+const RED_CUBES: u32 = 12;
+const BLUE_CUBES: u32 = 13;
+const GREEN_CUBES: u32 = 14;
+
+fn part1(buffer: &Vec<String>) -> u32 {
+    let mut game_id_sum:u32 = 0;
+    for line in buffer {
+        let game_str = line.split(":").collect::<Vec<&str>>()[0];
+        let color_str = line.split(":").collect::<Vec<&str>>()[1]
+            .strip_prefix(" ")
+            .unwrap();
+        let game_id = game_str.split(" ").collect::<Vec<&str>>()[1].parse::<u32>().unwrap();
+
+        // println!("{:?}", game_id.parse::<u32>().unwrap());
+        let color_combos: Vec<&str> = color_str.split(";").collect::<Vec<&str>>();
+        let mut add = true;
+
+        for combo in color_combos {
+            let combo_strs = combo.split(",").map(|x| x.trim()).collect::<Vec<&str>>();
+            let color_counts:Vec<(u32,Color)> = combo_strs
+                .iter()
+                .map(|x| {
+                    let mapping:Vec<&str> = x.split(" ").collect(); 
+                    (mapping[0].parse::<u32>().unwrap(), match mapping[1] {
+                        "green" => Color::Green,
+                        "blue" => Color::Blue,
+                        "red" => Color::Red,
+                        _ => Color::Red,
+                    })
+                }).collect();
+            for (count, color) in color_counts{
+                if color == Color::Blue{
+                    if count > BLUE_CUBES{
+                        add = false;
+                        break;
+                    }
+                }
+                if color == Color::Green {
+                    if count > GREEN_CUBES{
+                        add = false;
+                        break;
+                    }
+                }
+                if color == Color::Red {
+                    if count > RED_CUBES{
+                        add = false;
+                        break;
+                    }
+                }
+            }
+            if !add{
+                break;
+            }
+
+        }
+        if add{
+            game_id_sum = game_id_sum + game_id;
+        }
+    }
+    game_id_sum
+
+}
+
+
 fn part2(_buffer: &Vec<String>) -> u32 {
     0
 }
 
 fn main() {
     // File hosts.txt must exist in the current path
-    if let Ok(lines) = read_lines("./sample-1") {
+    if let Ok(lines) = read_lines("./input") {
         // Consumes the iterator, returns an (Optional) String
         let buffer: Vec<String> = lines.flatten().map(String::from).collect();
         println!("{}", part1(&buffer));
